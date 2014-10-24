4a563ad4f343ad650edf6133db23d2e246ef0931
alejo cortés <eacortesc@gmail.com>
2014-10-24 15:51:28 -0500

    avance python grafica pdf

---
+++ plot_poblaciones.py
@@ -0,0 +1,21 @@
+#Este programa grafica los resultados del archivo rungekutta_cazadoresPresas_1.c
+import sys
+import pylab
+import numpy as np
+
+#recogemos el nombre del archivo.dat que entra como parametro
+nombredatos = sys.argv[1]
+
+#cargar datos del archivo
+datos = np.loadtxt(nombredatos)
+
+#definir arreglos para graficar
+x = datos[:,1]
+y = datos[:,2]
+
+#Graficar
+pylab.plot(x,y)
+pylab.xlabel('X')
+pylab.ylabel('Y')
+pylab.title('Poblaciones x-y')
+pylab.savefig('grafica.pdf')
