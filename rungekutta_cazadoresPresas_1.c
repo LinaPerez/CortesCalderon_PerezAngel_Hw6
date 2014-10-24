bb25f449932100d2b6b81ecf484cfbc997771737
Lina Perez <lina.perezangel@gmail.com>
2014-10-23 21:39:54 -0500

    primeras 3 lineas

---
+++ rungekutta_cazadoresPresas_1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.b>
+
+
+int main (int argc, char **argv){
+  int i;
+  int j;
+  
+  float h = 0.01;
+  float min_t = 0.0;
+  float max_t = 1.0;
+  float n_points = ((max_t - min_t) / h);
+
+
+
+
+  return 0;
+}
