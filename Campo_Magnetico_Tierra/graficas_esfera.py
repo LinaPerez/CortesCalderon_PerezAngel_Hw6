3abb5d456c2c4e326899b3c6262bc9800c38b612
Lina Perez <lina.perezangel@gmail.com>
2014-10-29 21:33:02 -0500

    graficas

---
+++ Campo_Magnetico_Tierra/graficas_esfera.py
@@ -0,0 +1,40 @@
+#Este programa grafica los resultados del archivo particula_campo.c
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+
+#recogemos el nombre del archivo.dat que entra como parametro
+datosparticula = sys.argv[1]
+
+#cargar datos del archivo
+datos = np.loadtxt(datosparticula)
+
+#definir arreglos para graficar
+x = datos[:,1]
+y = datos[:,2]
+z = datos[:,3]
+
+#saber cuales fueron las condiciones iniciales
+l=list(datosparticula)
+n=len(l)
+l[n-4:n]=[]
+nombredatos="".join(l)
+
+#Graficar primero al trayectoria en x-y
+plt.plot(x,y)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Trayectoria X-Y')
+plt.savefig(str(nombredatos)+'.pdf')
+
+#Gracia 3D de la trayectoria en el plano x, y, z
+figura = figure()
+tp = Axes3D(figura)
+tp.set_xlabel("$X$",fontsize=20)
+tp.set_ylabel("$Y$",fontsize=20)
+tp.set_zlabel("$Z$",fontsize=20)
+tp.set_title("$\mathrm{Trayectoria Particula en el Campo Magnético de la Tierra}$", fontsize=30)
+tp.plot(x, y, z)
+plt.savefig(str(nombredatos)+'_3D_'+'.pdf')
+
